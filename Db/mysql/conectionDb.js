6cd8b5083ac3e371f7bf6f2137b709008c4f3902
federicomateucci <federicomateucci@gmail.com>
2022-03-03 20:13:16 -0300

    configdb

--- Db/mysql/conectionDb.js
+++ Db/mysql/conectionDb.js
@@ -1,9 +1,9 @@
 const { type } = require("express/lib/response");
 const Sequelize = require("sequelize")
 const{DataTypes}=Sequelize;
+require('dotenv').config()
 
-
-let dbDisney = new Sequelize("disneyapi","root","Popeye101#",{host:"localhost",
+let dbDisney = new Sequelize(process.env.DB_DATABASE,process.env.DB_USERNAME,process.env.DB_PASSWORD,{host:process.env.DB_HOST,
 dialect:"mysql"})
 
 
